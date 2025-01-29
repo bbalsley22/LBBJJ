7aa53a6bb3a1557fdbeb718f293043368d706749
parascape <brendan.balsley22@gmail.com>
2025-01-29 14:11:37 -0800

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { HeroSection } from "./components/HeroSection";
 import Auth from "./pages/Auth";
 import ManageSubscription from "./pages/ManageSubscription";
